c2c567cd2841425e52a6ef29e9db5e1c5172af9a
var-bin <var-bin@users.noreply.github.com>
2017-04-25 23:04:12 +0300

    Fix entry object. Add COMMON_NAME constant. Use CommonsChunkPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,14 @@ const NODE_ENV = process.env.NODE_ENV || DEV_ENV;
 const SRC_PATH = path.join(__dirname, "src")
 const DEST_PATH = path.join(__dirname, "public");
 
+const COMMON_NAME = "common";
+
 module.exports = {
   context: SRC_PATH,
 
   entry: {
     home: "./home/home",
-    about: "./about/about",
-    welcome: "./welcome/welcome"
+    about: "./about/about"
   },
 
   output: {
@@ -39,6 +40,9 @@ module.exports = {
       NODE_ENV: JSON.stringify(NODE_ENV),
       DEV_ENV: JSON.stringify(DEV_ENV),
       LANG: '"ru"'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: COMMON_NAME
     })
   ],
 
