5efd5c4e46f0d3aeed736ec72eb099897174d776
var-bin <var-bin@users.noreply.github.com>
2017-07-21 18:22:31 +0300

    Added configs for ExtractTextPlugin

--- v3/webpack.config.js
+++ v3/webpack.config.js
@@ -1,16 +1,33 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: path.resolve("./src/index.ts"),
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader"
+            }
+          ]
+        })
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "[name].css"
+    })
+  ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"]
   },
