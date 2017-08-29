b858361d47e8ffc8f95a017a0697fdff5eae088e
var-bin <var-bin@users.noreply.github.com>
2017-08-29 19:03:53 +0300

    Added new settings

--- customApp/webpack.config.js
+++ customApp/webpack.config.js
@@ -5,9 +5,84 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "build")
+    path: path.join(__dirname, "build")
+  },
+  module: {
+    rules: [
+      /* typescript */
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+
+      /* css */
+
+      {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader"
+        }]
+      },
+
+      /* scss */
+      {
+        test: /\.scss$/,
+        use: [
+        {
+          loader: "style-loader" // creates style nodes from JS strings
+        },
+        {
+          loader: "css-loader" // translates CSS into CommonJS
+        },
+        {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+
+      /* images */
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "images/[name].[ext]"
+          }
+        }]
+      },
+
+      /* fonts */
+      {
+        test: /\.(svg|woff|woff2|eot|ttf|otf)$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]"
+          }
+        }]
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, "src"),
+      "node_modules"
+    ],
+    extensions: [
+      ".tsx",
+      ".ts",
+      ".js"
+    ]
+  },
+  resolveLoader: {
+    modules: [
+      path.join(__dirname, "node_modules")
+    ]
   }
 };
