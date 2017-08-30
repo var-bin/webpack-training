4195bcb4f3ca6e197f57e96a401adf180a679552
var-bin <var-bin@users.noreply.github.com>
2017-08-30 18:27:49 +0300

    Added LodashModuleReplacementPlugin. Customize UglifyJSPlugin

--- customApp/buildUtils/webpack.prod.js
+++ customApp/buildUtils/webpack.prod.js
@@ -5,13 +5,21 @@
 const os = require("os");
 
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 
 const config = {
   plugins: [
+    new LodashModuleReplacementPlugin(),
+
     new UglifyJSPlugin({
       parallel: {
         cache: true,
         workers: os.cpus().length
+      },
+      uglifyOptions: {
+        compress: {
+          drop_console: true
+        }
       }
     })
   ]
