a4808f9f4aa72f77a77e7a776b8d85bc3d3a83e4
var-bin <var-bin@users.noreply.github.com>
2017-09-01 15:55:39 +0300

    Added examples for lazyload

--- customApp/src/app.ts
+++ customApp/src/app.ts
@@ -1,4 +1,21 @@
 // app.ts
 
-import "./app.module";
-import "./app.myController";
+/* require.ensure([
+  "./myFirstModule/app.module",
+  "./myFirstModule/app.myController",
+  "./myFirstModule/styles.scss"
+], (require) => {
+  require("./myFirstModule/app.module");
+  require("./myFirstModule/app.myController");
+  require("./myFirstModule/styles.scss");
+}); */
+
+/* import "./myFirstModule/app.module";
+import "./myFirstModule/app.myController";
+import "./myFirstModule/styles.scss"; */
+
+require.ensure([], () => {
+  require("./myFirstModule/app.module");
+  require("./myFirstModule/app.myController");
+  require("./myFirstModule/styles.scss");
+});
