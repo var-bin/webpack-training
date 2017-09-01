64b33856c869ddb2fe1e59d1bf278444dd986a57
var-bin <var-bin@users.noreply.github.com>
2017-09-01 15:57:22 +0300

    Delete useless console.log

--- customApp/webpack.config.js
+++ customApp/webpack.config.js
@@ -5,12 +5,7 @@
 const commonConfig = require("./buildUtils/webpack.common");
 const webpackMerge = require("webpack-merge");
 
-const ENV = process.env.NODE_ENV;
-console.log(`ENV: ${ENV}`);
-
 module.exports = (env) => {
-  console.log(env);
-
   const envConfig = require(`./buildUtils/webpack.${env}`);
 
   return webpackMerge(commonConfig, envConfig);
