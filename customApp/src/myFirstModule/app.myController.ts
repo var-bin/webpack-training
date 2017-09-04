a921ee0a7fe575a5138c9a9810aa06116504d118
var-bin <var-bin@users.noreply.github.com>
2017-09-04 17:11:55 +0300

    Work on pdfMake. Fail (

--- customApp/src/myFirstModule/app.myController.ts
+++ customApp/src/myFirstModule/app.myController.ts
@@ -1,6 +1,12 @@
 import * as angular from "angular";
 const moment = require("moment") as any;
 
+const pdfMake = require("pdfmake.js") as any;
+
+window["pdfMake"] = pdfMake;
+const vfs = require("vfs_fonts.js");
+window["pdfMake"]["vfs"] = vfs;
+
 class MyController {
   public sayHi() {
     const now = moment().format("LLLL");
@@ -9,7 +15,11 @@ class MyController {
   }
 
   public onClick() {
-    this.sayHi();
+    const docDefinition = {
+      content: "This is an sample PDF printed with pdfMake"
+    };
+
+    pdfMake.createPdf(docDefinition).open();
   }
 }
 
