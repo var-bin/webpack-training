418646ec5c55b12f7cf26d3f3bb5726d3b1cebd8
var-bin <var-bin@users.noreply.github.com>
2017-09-04 14:28:29 +0300

    Delete useless require

--- customApp/buildUtils/webpack.common.js
+++ customApp/buildUtils/webpack.common.js
@@ -2,8 +2,6 @@
 
 "use strict";
 
-const path = require("path");
-
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
