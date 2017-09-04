835af358b1e1ab970bea09c1d0bc62c2a3722781
var-bin <var-bin@users.noreply.github.com>
2017-09-04 12:34:37 +0300

    Use moment.js

--- customApp/src/myFirstModule/app.myController.ts
+++ customApp/src/myFirstModule/app.myController.ts
@@ -1,8 +1,11 @@
 import * as angular from "angular";
+const moment = require("moment") as any;
 
 class MyController {
   public sayHi() {
-    return "Say hi from AngularJS!";
+    const now = moment().format("LLLL");
+
+    return `Say hi from AngularJS! ${now}`;
   }
 
   public onClick() {
